6595484e702370b8cb76bf21343e081c74051a5f
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-08-17 12:21:51 +0200

    Auth : (Models) re generating users model

--- models/user.js
+++ models/user.js
@@ -1,32 +1,21 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('users', {
-    firstname: DataTypes.STRING,
-    lastname: DataTypes.STRING,
-    phone: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
     username: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     token: DataTypes.STRING,
     favorite_item: DataTypes.INTEGER,
-    max_price: DataTypes.DECIMAL,
+    max_price: DataTypes.FLOAT,
     favorite_transportation: DataTypes.INTEGER,
     created_by: DataTypes.INTEGER,
-    created_at: DataTypes.DATE,
     updated_by: DataTypes.INTEGER,
-    updated_at: DataTypes.DATE,
-    isActive: DataTypes.TINYINT,
+    is_active: DataTypes.BOOLEAN
   }, {});
   User.associate = function(models) {
-    User.belongsTo(models.UserType, {
-      foreignKey: 'user_type_id',
-    });
-    User.belongsTo(models.Address, {
-      foreignKey: 'addresses_id',
-    });
-    User.belongsTo(models.Picture, {
-      foreignKey: 'pictures_id',
-    });
+    // associations can be defined here
   };
   return User;
 };
