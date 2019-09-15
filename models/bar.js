fb374fc919a58988219413443d0f415f9107cc3f
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-09-15 18:46:58 +0200

    Bar : Creating the bar model

---
+++ models/bar.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Bar = sequelize.define('bar', {
+    name: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    type: DataTypes.STRING,
+    created_by: DataTypes.INTEGER,
+    created_at: DataTypes.DATE,
+    updated_by: DataTypes.INTEGER,
+    updated_at: DataTypes.DATE,
+    is_active: DataTypes.TINYINT,
+  }, {
+    timestamps: false
+  });
+  Bar.associate = function(models) {
+    Bar.belongsTo(models.address, {
+      foreignKey: 'addresses_id',
+    });
+    Bar.belongsTo(models.user, {
+      foreignKey: 'users_id',
+    });
+  };
+};
