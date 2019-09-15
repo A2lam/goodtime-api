66bc8a851435d390ba0315acf7c19a6df5992f55
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-09-15 23:23:17 +0200

    Bar : Creating good times models

---
+++ models/goodtime.js
@@ -0,0 +1,25 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const GoodTime = sequelize.define('good_time', {
+    is_booking: DataTypes.TINYINT,
+    number_of_participants: DataTypes.INTEGER,
+    status: DataTypes.STRING,
+    refusal_purpose: DataTypes.STRING,
+    created_by: DataTypes.INTEGER,
+    created_at: DataTypes.DATE,
+    updated_by: DataTypes.INTEGER,
+    updated_at: DataTypes.DATE,
+    is_active: DataTypes.TINYINT,
+  }, {
+    timestamps: false
+  });
+  GoodTime.associate = function(models) {
+    GoodTime.belongsTo(models.group, {
+      foreignKey: 'groups_id',
+    });
+    GoodTime.belongsTo(models.bar, {
+      foreignKey: 'bars_id',
+    });
+  };
+  return GoodTime;
+};
