9f9751b18dea3b6896d6830c6abfbeaa28a3eb1b
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-09-16 23:37:37 +0200

    Bar : Implementing update bar functions

--- src/bar/services/update.js
+++ src/bar/services/update.js
@@ -6,7 +6,7 @@ export default function (req, res, next) {
       id: req.user.id
     }
   }).then(user => {
-    return models.address.create({
+    return models.address.update({
       number: req.body.number,
       street: req.body.street,
       complement: req.body.complement,
@@ -14,20 +14,27 @@ export default function (req, res, next) {
       city: req.body.city,
       latitude: req.body.latitude,
       longitude: req.body.longitude
+    }, {
+      where : {
+        id: req.body.addresses_id,
+      }
     }).then(address => {
-      return models.bar.create({
+      return models.bar.update({
         name: req.body.name,
         addresses_id: address.id,
         owned_id: user.id,
         phone: req.body.phone,
         type: req.body.type,
-        created_by: user.id,
-        is_active: 1
+        updated_by: user.id,
+      }, {
+        where: {
+          id: req.params.id,
+        }
       }).then(bar => {
         res.send({bar, address})
       }).catch(err => {
         next(err);
       })
     });
-  });
+  })
 }
